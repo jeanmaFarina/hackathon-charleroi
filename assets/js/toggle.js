8e6fd013cd75d4723e99be6347d85db16f2c1f08
Abel <98821906+AbielKiflu@users.noreply.github.com>
2022-03-20 07:56:38 +0100

    Delete toggle.js

--- assets/js/toggle.js
+++
@@ -1,7 +0,0 @@
-let toggle = document.querySelector(".toggle");
-let nav = document.querySelector(".nav");
-
-
-toggle.addEventListener("click", function(){
-    nav.classList.toggle("toggle__on");
-});
